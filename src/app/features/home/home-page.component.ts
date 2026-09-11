c423be1c3a2c3eb42acda37febcf7ad2bf01b238
Tajudeen WWariz <thabidemi@gmail.com>
2026-09-11 12:36:01 +0100

    feat: added leadeship board to home

--- src/app/features/home/home-page.component.ts
+++ src/app/features/home/home-page.component.ts
@@ -6,10 +6,11 @@ import { PUBLIC_SITE_CONFIG } from '../../core/config/public-site-config.token';
 import { AuthStateService } from '../../core/services/auth-state.service';
 import { HealthCheckPackagesApiService } from '../../core/services/health-check-packages-api.service';
 import { formatMinor } from '../provider/care-money';
+import { CommunityLeaderboardComponent } from '../join/components/community-leaderboard/community-leaderboard.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink],
+  imports: [RouterLink, CommunityLeaderboardComponent],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
